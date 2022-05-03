cae53d2a181384b79982aab89ce372328e35092e
Ramansh27 <rchhabra6@my.bcit.ca>
2022-05-03 09:56:02 -0800

    Created routes for products, admin and about page
    Modified app.py to include routed for products, admin login and about us page.

--- app.py
+++ app.py
@@ -6,5 +6,17 @@ app = Flask(__name__)
 def homepage():
     return render_template('/home.html')
 
+@app.route('/products')
+def products():
+    return '<h1>Products Page</h1>'
+
+@app.route('/admin')
+def admin():
+    return '<h1>Admin Login Page</h1>'
+
+@app.route('/about')
+def about():
+    return '<h1>About our One Stop Shop</h1>'
+
 if __name__ == "__main__":
     app.run(debug=True)
