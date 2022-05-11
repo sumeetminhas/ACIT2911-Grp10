aea9cc5a2bff4c31bbb1554a05012c93b0d4eecb
Yingyi He <yhe77@my.bcit.ca>
2022-05-10 19:18:02 -0700

    Add side bar for about page

---
+++ static/app.js
@@ -0,0 +1,16 @@
+const btnCart = document.querySelector('#cart-logo')
+const navContent = document.querySelector('#side-content')
+const btnBack = document.querySelector('#back')
+
+navContent.style.display = 'none'
+
+btnCart.addEventListener('click',openSideNav)
+btnBack.addEventListener('click',closeSideNav)
+function openSideNav(){
+    navContent.style.display = 'block'
+    btnCart.style.display = "none"
+}
+function closeSideNav(){
+    navContent.style.display = 'none'
+    btnCart.style.display = "block"
+}
