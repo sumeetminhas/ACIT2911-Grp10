87c7519e1d019e48b09a5fdfa9f7cfda791f1dd8
Ramansh27 <rchhabra6@my.bcit.ca>
2022-05-25 07:59:03 -0800

    added functions to modify json

--- functions.py
+++ functions.py
@@ -1,6 +1,7 @@
 import csv
 import json
 import os
+from cart import Cart
 from werkzeug.utils import secure_filename
 
 products_file_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'admin-only')
@@ -11,13 +12,27 @@ def read_products():
     with open(os.path.join(products_file_path, 'products.csv'), 'r') as file:
             product_list = list(csv.reader(file))
             images = os.listdir('static/product_image')
+            image_list = []
+            for image in images:
+                image_list.append(image[:-4])
 
-            return product_list, images
+            return product_list, image_list
 
 def read_json():
     with open(transactions_file_path, 'r') as history:
         return json.load(history)
 
-def update_history(list):
-    all_history = read_json()
-    return none
+def add_to_history(transaction, history):
+    history.append(transaction)
+    with open(transactions_file_path, 'w') as file:
+        json.dump(history, file)
+
+def class_to_dict(instance):
+    if isinstance(instance, Cart):
+        list_html = "<ul class='final-order'>"
+        for item in instance.list:
+            li_item = f'<div>{item[1]}: {item[2]}</div>'
+            list_html += li_item
+        list_html += "</ul>"
+
+        return list_html, instance.total
